c27e5fa18401e0e3b123d7cfdea5809726a441e9
Igor Petrovic <758832+igorovic@users.noreply.github.com>
2023-01-25 15:04:29 +0100

    build(test): test 2

--- .husky/common.sh
+++ .husky/common.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 command_exists () {
   command -v "$1" >/dev/null 2>&1
 }
-
 # Workaround for Windows 10, Git Bash and Yarn
 # to avoid errors when using /dev/tty in hooks
 if command_exists winpty && test -t 1; then
